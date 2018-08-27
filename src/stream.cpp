c0d61d32d4708e6516aa5097bae0035a5abc1c0d
ReinUsesLisp <reinuseslisp@airmail.cc>
2018-08-26 22:27:33 -0300

    Fixup License

--- src/stream.cpp
+++ src/stream.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the sirit project.
  * Copyright (c) 2018 ReinUsesLisp
  * This software may be used and distributed according to the terms of the GNU
- * General Public License version 2 or any later version.
+ * Lesser General Public License version 2 or any later version.
  */
 
 #include "stream.h"
