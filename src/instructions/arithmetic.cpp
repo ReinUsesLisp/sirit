60a856d2669bde23f9a4a0fb3879ec89a16d904e
ReinUsesLisp <reinuseslisp@airmail.cc>
2019-07-14 18:48:59 -0300

    Relicense to The BSD 3-clause license

--- src/instructions/arithmetic.cpp
+++ src/instructions/arithmetic.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the sirit project.
- * Copyright (c) 2018 ReinUsesLisp
- * This software may be used and distributed according to the terms of the GNU
- * Lesser General Public License version 3 or any later version.
+ * Copyright (c) 2019 sirit
+ * This software may be used and distributed according to the terms of the
+ * 3-Clause BSD License
  */
 
 #include <memory>
